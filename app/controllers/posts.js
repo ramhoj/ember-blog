dd0227375ec11bc70fe44dc169f78b50bd86c8ea
Nicklas Ramhöj Holtryd <ramhoj@gmail.com>
2025-09-09 10:03:13 +0200

    Remove delay and use taks variant over options

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -2,7 +2,7 @@ import Controller from "@ember/controller"
 import { service } from "@ember/service"
 import { action } from "@ember/object"
 import { tracked } from "@glimmer/tracking"
-import { task, timeout } from "ember-concurrency"
+import { restartableTask, timeout } from "ember-concurrency"
 
 export default class PostsController extends Controller {
   @service store
@@ -18,11 +18,10 @@ export default class PostsController extends Controller {
     this.searchTask.perform(this.qInput)
   }
 
-  searchTask = task({ restartable: true }, async (query) => {
-    await timeout(300)                  // debounce
-    this.q = query                      // sync URL after debounce
+  searchTask = restartableTask(async (query) => {
+    this.q = query
     let params = this.q ? { q: this.q } : {}
     let results = await this.store.query("post", params)
-    this.rows = results                 // last task wins (prior ones canceled)
+    this.rows = results
   })
 }
