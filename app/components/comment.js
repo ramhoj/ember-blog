8f7f5f85ba3fac8a59c399fae5ab75ffe9adb6b5
Nicklas Ramhöj Holtryd <ramhoj@gmail.com>
2025-09-17 11:10:11 +0200

    Move parent assignment to on save
    …as the constructor is only run once per component type (not per
    <CommentForm> invocation).

--- app/components/comment.js
+++ app/components/comment.js
@@ -5,11 +5,6 @@ import { action } from "@ember/object"
 export default class CommentComponent extends Component {
   @tracked isEditing = false
 
-  constructor() {
-    super(...arguments)
-    this.args.comment.post = this.args.post
-  }
-
   @action edit() {
     this.isEditing = true
   }
@@ -20,7 +15,9 @@ export default class CommentComponent extends Component {
   }
 
   @action async update({ body }) {
+    this.args.comment.post = this.args.post
     this.args.comment.body = body
+
     await this.args.comment.save()
     this.isEditing = false
   }
